df335fe9933397017ed7a1644d47e749dbae5941
Verena Isele <v.j.isele@gmail.com>
2021-02-03 20:08:11 +0100

    Verschönerung und svg gefärbt

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-//button blendet Antwort ein
-const button = document.querySelector('#button1')
-const answer = document.querySelector('#answer1')
+//button blendet Antwort ein; Lösung mit IDs
+//const button = document.querySelector('#button1')
+//const answer = document.querySelector('#answer1')
+//button.addEventListener('click', () => {
+    // answer.classList.toggle('hidden')
+//})
 
-button.addEventListener('click', () => {
-    answer.classList.toggle('hidden')
+//Für alle Buttons auf einmal anwenden
+const buttons = document.querySelectorAll('.card__button');
+
+buttons.forEach((mybutton) => {
+    mybutton.addEventListener('click', () => {
+        const answer = mybutton.parentNode.querySelector('.card__answer');
+        answer.classList.toggle('hidden');
+
+        mybutton.innerText = answer.classList.contains('hidden')
+        ? 'Lösung anzeigen'
+        : 'Lösung verbergen';
+    })
 })
-
-const button2 = document.querySelector('#button2')
-const answer2 = document.querySelector('#answer2')
-
-button2.addEventListener('click', () => {
-    answer2.classList.toggle('hidden')
-})
-
-//Bookmark gefüllt oder leer
+       
+//
